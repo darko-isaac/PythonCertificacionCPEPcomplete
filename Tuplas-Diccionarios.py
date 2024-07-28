74fe072d7e56d0757f2bd63a121f42b68a382da8
emmanuel isaac Garcia Cruz <isaac.garcia.cruz@gmail.com>
2024-07-28 12:11:31 -0600

    Primer commit: Programas por 4 modulos

---
+++ Tuplas-Diccionarios.py
@@ -0,0 +1,30 @@
+school_class = {}
+
+while True:
+    name = input("Ingresa el nombre del estudiante: ")
+    if name == '':
+        break
+
+    score = int(input("Ingresa la calificación del estudiante (0-10): "))
+    if score not in range(0, 11):
+        break
+
+    if name in school_class:
+        school_class[name] += (score,2)
+    else:
+        school_class[name] = (score,)
+
+for name in sorted(school_class.keys()):
+    adding = 0
+    counter = 0
+    for score in school_class[name]:
+        adding += score
+        counter += 1
+    print(name, ":", adding / counter)
+
+print(sorted(school_class))
+
+dic = {'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 4, 'f': 5, 'g': 6, 'h': 2, 'i': 7, 'j': 2, 'k': 8, 'l': 9}
+#esta linea de codigo hace uso del count en diccionarios para los valores contenidos en sus keys
+duplicates = list(dic.values()).count(2)
+print(duplicates)  # Output: 4
